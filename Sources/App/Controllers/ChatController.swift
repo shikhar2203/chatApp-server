77a26ed17cbb52dfe25ae60ffcd462e3b0489a8b
shikhar2203 <shikharsharma2203@gmail.com>
2023-10-04 22:48:32 +0530

    added chat controller

---
+++ Sources/App/Controllers/ChatController.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by shikhar on 04/10/23.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+struct ChatController: RouteCollection {
+    
+    func boot(routes: Vapor.RoutesBuilder) throws {
+        
+    }
+    
+    
+}
+
